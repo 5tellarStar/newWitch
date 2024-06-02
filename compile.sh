09a789ecd9695ce86b3a85345fb8ceae96baa44b
5tellarStar <skold2@outlook.com>
2024-06-02 12:20:33 +0200

    just somethings

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 rm testBuild
 g++ main.cpp -o testBuild -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
 ./testBuild
--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 
 
 #include <SFML/Graphics.hpp>
-
+float screenWidth = 240.f;
+float screenHeight = 144.f;
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "New Witch");
+    sf::RenderWindow window(sf::VideoMode(240, 144), "New Witch");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -18,6 +19,24 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::Resized)
+            {
+                sf::Vector2u size = window.getSize();
+
+                float heightRatio = screenHeight / screenWidth;
+                float widthRatio = screenWidth / screenHeight;
+
+                if (size.y * widthRatio <= size.x)
+                {
+                    size.x = size.y * widthRatio;
+                }
+                else if (size.x * heightRatio <= size.y)
+                {
+                    size.y = size.x * heightRatio;
+                }
+
+                window.setSize(size);
+            }
         }
 
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
