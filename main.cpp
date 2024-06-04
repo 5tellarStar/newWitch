3c1d736e405714c4578a6e99214a30a6f963ff74
5tellarStar <skold2@outlook.com>
2024-06-04 23:03:59 +0200

    kinda made movement

--- main.cpp
+++ main.cpp
@@ -4,16 +4,28 @@
 
 int playerState = 0;
 
-int map[][] = 
+float playerX = 3;
+int oldPlayerX = 3;
+float playerY = 5;
+int oldPlayerY = 5;
+
+int playerDir = 2;
+
+bool animation = false;
+
+bool walking = false;
+
+bool canWalk = true;
+
+int map[6][10] = 
 {
-    {1,1,1,1,1,1,1},
-    {1,0,0,0,0,0,1},
-    {1,0,0,0,0,0,1},
-    {1,0,0,0,0,0,1},
-    {1,0,0,0,0,0,1},
-    {1,0,0,0,0,0,1},
-    {1,1,1,1,1,1,1}
-}
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1}
+};
 
 int main()
 {
@@ -22,9 +34,10 @@ int main()
     sf::Texture texture;
     texture.loadFromFile("Witch.png");
     
-    sf::IntRect rectSourcePlayer(1920,0,40,40);
-    sf::Sprite sprite(texture, rectSourceSprite);
-    sf::Clock clock;
+    sf::IntRect rectSourcePlayer(1280,0,40,40);
+    sf::Sprite playerSprite(texture, rectSourcePlayer);
+    sf::Clock playerTime;
+    sf::Clock walkDelay;
 
     while (window.isOpen())
     {
@@ -35,7 +48,173 @@ int main()
                 window.close();
         }
 
-        if(clock.getElapsedTime().asSeconds() > 1.0f)
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !walking && canWalk)
+        {
+            if(playerDir != 0 || walkDelay.getElapsedTime().asSeconds() < 0.1f)
+            {
+                playerDir = 0;
+                switch (playerState)
+                {
+                case 0:
+                    rectSourcePlayer.left = 1400;
+                    break;
+            
+                default:
+                    break;
+                }
+                walkDelay.restart();
+            }
+            else
+            {
+                walking = true;
+                animation = false;
+                playerTime.restart();
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !walking && canWalk)
+        {
+            if(playerDir != 1 || walkDelay.getElapsedTime().asSeconds() < 0.1f)
+            {
+                playerDir = 1;
+                switch (playerState)
+                {
+                case 0:
+                    rectSourcePlayer.left = 1520;
+                    break;
+                default:
+                    break;
+                }
+                walkDelay.restart();
+            }
+            else
+            {
+                walking = true;
+                animation = false;
+                playerTime.restart();
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && !walking && canWalk)
+        {
+            if(playerDir != 2 || walkDelay.getElapsedTime().asSeconds() < 0.1f)
+            {
+                playerDir = 2;
+                switch (playerState)
+                {
+                case 0:
+                    rectSourcePlayer.left = 1280;
+                    break;
+            
+                default:
+                    break;
+                }
+                walkDelay.restart();
+            }
+            else
+            {
+                walking = true;
+                animation = false;
+                playerTime.restart();
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && !walking && canWalk)
+        {
+            if(playerDir != 3 || walkDelay.getElapsedTime().asSeconds() < 0.1f)
+            {
+                playerDir = 3;
+                switch (playerState)
+                {
+                case 0:
+                    rectSourcePlayer.left = 1640;
+                    break;
+            
+                default:
+                break;
+                }
+                walkDelay.restart();
+            }
+            else
+            {
+                walking = true;
+                animation = false;
+                playerTime.restart();
+            }
+        } 
+
+        switch (playerState)
+        {
+        case 0:
+            if(walking)
+            {
+                switch (playerDir)
+                {
+                case 0:
+                    playerY = oldPlayerY - playerTime.getElapsedTime().asSeconds();
+                    break;
+                case 1:
+                    playerX = oldPlayerX + playerTime.getElapsedTime().asSeconds();
+                    break;
+                case 2:
+                    playerY = oldPlayerY + playerTime.getElapsedTime().asSeconds();
+                    break;
+                case 3:
+                    playerX = oldPlayerX - playerTime.getElapsedTime().asSeconds();
+                    break;
+                default:
+                    break;
+                }
+
+                if(playerTime.getElapsedTime().asSeconds() > 1.0f)
+                {
+                    rectSourcePlayer.left -= 80;
+                    switch (playerDir)
+                    {
+                    case 0:
+                        oldPlayerY--;
+                        break;
+                    case 1:
+                        oldPlayerX++;
+                        break;
+                    case 2:
+                        oldPlayerY++;
+                        break;
+                    case 3:
+                        oldPlayerX--;
+                        break;
+                    
+                    default:
+                        break;
+                    }
+                    playerY = oldPlayerY;
+                    walking = false;
+                }
+                else if (playerTime.getElapsedTime().asSeconds() > 0.75f && !animation)
+                {
+                    rectSourcePlayer.left += 80;
+                    animation = true;
+                }
+                else if (playerTime.getElapsedTime().asSeconds() > 0.50f && playerTime.getElapsedTime().asSeconds() < 0.75f && animation)
+                {
+                    rectSourcePlayer.left -= 40;
+                    animation = false;
+                }
+                else if (playerTime.getElapsedTime().asSeconds() > 0.25f && playerTime.getElapsedTime().asSeconds() < 0.50f && !animation)
+                {
+                    rectSourcePlayer.left += 40;
+                    animation = true;
+                }
+            }
+            break;
+        
+        default:
+            break;
+        }
+
+
+
+
+        /*
+        if(playerTime.getElapsedTime().asSeconds() > 1.0f)
         {
             if (rectSourcePlayer.left == 1960)
             {
@@ -44,13 +223,41 @@ int main()
             else
                 rectSourcePlayer.left += 40;
 
-            sprite.setTextureRect(rectSourcePlayer);
-            clock.restart();
+            playerSprite.setTextureRect(rectSourcePlayer);
+            playerTime.restart();
         }
+        */
+
+
+
+
+
+
+
+
 
         window.clear();
-        window.draw(sprite);
+        playerSprite.setTextureRect(rectSourcePlayer);
+        playerSprite.setPosition(sf::Vector2f(playerX*24 - 8,playerY*24 - 20));
+
+        sf::RectangleShape tile(sf::Vector2f(24, 24));
+        tile.setFillColor(sf::Color::White);
+
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
+                {
+                    tile.setPosition(sf::Vector2f(i * 24, j * 24));
+                    window.draw(tile);
+                }
+            }
+        }
+
+        window.draw(playerSprite);
         window.display();
+        
     }
 
     return 0;
